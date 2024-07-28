0ec55417319cdb1ac50c3ca2fd993486d64d57be
Ilaria <ilaria.frandina@gmail.com>
2024-07-28 13:36:21 +0200

    aggiunto mainMenu + canvas per le opzioni

--- EnchantedVillageClient/Assets/Scripts/BuildGrid.cs
+++ EnchantedVillageClient/Assets/Scripts/BuildGrid.cs
@@ -12,12 +12,12 @@ namespace Unical.Demacs.EnchantedVillage
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.white;
-            for(int k=0; k<=_nRows-1; k++)
+            for(int k=0; k<=_nRows; k++)
             {
                 Vector3 point=transform.position+transform.forward.normalized*_cellSize*(float)k;
                 Gizmos.DrawLine(point, point+transform.right.normalized*_cellSize*(float)_nCols);
             }
-            for (int k = 0; k <= _nCols - 1; k++)
+            for (int k = 0; k <= _nCols; k++)
             {
                 Vector3 point = transform.position + transform.right.normalized * _cellSize * (float)k;
                 Gizmos.DrawLine(point, point + transform.forward.normalized * _cellSize * (float)_nRows);
