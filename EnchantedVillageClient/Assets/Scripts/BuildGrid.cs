6c4e181c429cc1122bd8504cf1deabba847bd0ac
Ilaria <ilaria.frandina@gmail.com>
2024-07-28 11:52:56 +0200

    aggiunta griglia + base per gli edifici

---
+++ EnchantedVillageClient/Assets/Scripts/BuildGrid.cs
@@ -0,0 +1,28 @@
+namespace Unical.Demacs.EnchantedVillage
+{
+    using UnityEngine;
+
+    public class BuildGrid : MonoBehaviour
+    {
+        private int _nRows = 45;
+        private int _nCols = 45;
+        private float _cellSize = 1f;
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.white;
+            for(int k=0; k<=_nRows-1; k++)
+            {
+                Vector3 point=transform.position+transform.forward.normalized*_cellSize*(float)k;
+                Gizmos.DrawLine(point, point+transform.right.normalized*_cellSize*(float)_nCols);
+            }
+            for (int k = 0; k <= _nCols - 1; k++)
+            {
+                Vector3 point = transform.position + transform.right.normalized * _cellSize * (float)k;
+                Gizmos.DrawLine(point, point + transform.forward.normalized * _cellSize * (float)_nRows);
+            }
+        }
+    }
+#endif
+}
