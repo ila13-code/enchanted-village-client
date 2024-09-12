6f511029f67ddfdbcb7b7c4b21248367107a1d18
Ilaria <ilaria.frandina@gmail.com>
2024-09-12 11:58:03 +0200

    swordman animato

--- EnchantedVillageClient/Assets/Scripts/SwordManController.cs
+++ EnchantedVillageClient/Assets/Scripts/SwordManController.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class SwordManController : MonoBehaviour
 {
     private Animator animator;
+    private static readonly int IdleState = Animator.StringToHash("idle");
+    private static readonly int WalkState = Animator.StringToHash("walk");
+    private static readonly int AttackState = Animator.StringToHash("attack");
 
     void Start()
     {
-        // Ottiene il componente Animator
         animator = GetComponent<Animator>();
+    }
 
-        // Setta l'animazione iniziale su "idle"
-        animator.Play("idle");
+    public void PlayIdle()
+    {
+        animator.Play(IdleState);
+    }
+
+    public void PlayWalk()
+    {
+        animator.Play(WalkState);
+    }
+
+    public void PlayAttack()
+    {
+        animator.Play(AttackState);
     }
 }
