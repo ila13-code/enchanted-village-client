94785ee4ba8e3d86707066257ea705677e126785
Ilaria <ilaria.frandina@gmail.com>
2024-11-09 11:28:18 +0100

    fixx

--- EnchantedVillageClient/Assets/Scripts/UI/CashDialog.cs
+++ EnchantedVillageClient/Assets/Scripts/UI/CashDialog.cs
@@ -1,35 +1,38 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Threading.Tasks;
 
 namespace Unical.Demacs.EnchantedVillage
 {
     public class CashDialog : MonoBehaviour
     {
-        private TaskCompletionSource<bool> tcs;
+        private static TaskCompletionSource<bool> tcs;
 
-        public void Show()
+        public void YesClicked()
         {
-            gameObject.SetActive(true);
+            Debug.Log("Yes clicked");
+            if (tcs != null)
+            {
+                tcs.SetResult(true);
+            }
+            gameObject.SetActive(false);
+        }
+        public void NoClicked()
+        {
+            Debug.Log("No clicked");
+            if (tcs != null)
+            {
+                tcs.SetResult(false);
+            }
+            gameObject.SetActive(false);
+        }
+
+
+        public async Task<bool> ShowAndWait()
+        {
             tcs = new TaskCompletionSource<bool>();
-        }
-
-        public Task<bool> WaitForUserInput()
-        {
-            return tcs.Task;
-        }
-
-        public void OnConfirm()
-        {
-            Debug.Log("OnConfirm chiamato");
-            tcs.TrySetResult(true);
-            gameObject.SetActive(false);
-        }
-
-        public void OnCancel()
-        {
-            Debug.Log("OnCancel chiamato");
-            tcs.TrySetResult(false);
-            gameObject.SetActive(false);
+            gameObject.SetActive(true);
+            return await tcs.Task;
         }
     }
 }
--- EnchantedVillageClient/Assets/Scripts/UI/UIBuilding.cs
+++ EnchantedVillageClient/Assets/Scripts/UI/UIBuilding.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.UI;
-using static Unical.Demacs.EnchantedVillage.Building;
+using System.Threading.Tasks;
+
 namespace Unical.Demacs.EnchantedVillage
 {
     public class UIBuilding : MonoBehaviour
     {
-        [SerializeField]private int _prefabIndex = 0;
-        
+        [SerializeField] private int _prefabIndex = 0;
         private Transform buildingsContainer;
 
         private void Awake()
@@ -20,79 +15,52 @@ namespace Unical.Demacs.EnchantedVillage
             {
                 buildingsContainer = map.transform.Find("Buildings").transform;
             }
-            else
-            {
-                Debug.LogError("Map non trovato nella scena.");
-            }
         }
 
         public async void PlaceBuilding()
         {
             Debug.Log("Inizio PlaceBuilding");
+
             if (PlayerPrefsController.Instance.Gold < ShopItem.GetCostFromIndex(_prefabIndex))
             {
-                Debug.Log("L'utente non ha abbastanza risorse per acquistare l'edificio");
+                Debug.Log("Risorse insufficienti");
                 await ShowErrorDialog();
                 return;
             }
+
+            var cashDialog = UIController.Instance._dialogs.transform
+                .Find("CashDialog").GetComponent<CashDialog>();
+
+            bool confirmed = await cashDialog.ShowAndWait();
+
+            if (confirmed)
+            {
+                Debug.Log("Confermato, piazzo edificio");
+                PlayerPrefsController.Instance.Gold -= ShopItem.GetCostFromIndex(_prefabIndex);
+                PlayerPrefsController.Instance.Exp += ShopItem.GetExperiencePoints(_prefabIndex);
+                Vector3 position = Vector3.zero;
+                Building building = Instantiate(UIController.Instance.Buildings[_prefabIndex],
+                    position, Quaternion.identity, buildingsContainer);
+                building.Id = System.Guid.NewGuid().ToString();
+                building.UpdateGridPosition((int)position.x, (int)position.y);
+            }
             else
             {
-                bool confirmed = await ShowCashDialog();
-
-                if (confirmed)
-                {
-                    Debug.Log("Conferma ricevuta, piazzamento edificio.");
-                    PlayerPrefsController.Instance.Gold -= ShopItem.GetCostFromIndex(_prefabIndex);
-                    PlayerPrefsController.Instance.Exp += ShopItem.GetExperiencePoints(_prefabIndex);
-                    Vector3 position = Vector3.zero;
-                    Building building = Instantiate(UIController.Instance.Buildings[_prefabIndex], position, Quaternion.identity, buildingsContainer);
-                    building.Id= Guid.NewGuid().ToString();
-                    building.UpdateGridPosition((int)position.x, (int)position.y);
-                }
-                else
-                {
-                    Debug.Log("L'utente ha annullato il posizionamento dell'edificio.");
-                }
+                Debug.Log("Annullato");
             }
-        }
-
-        private async Task<bool> ShowCashDialog()
-        {
-            GameObject dialogInstance = UIController.Instance._dialogs.transform.Find("CashDialog").gameObject;
-            CashDialog dialog = dialogInstance.GetComponent<CashDialog>();
-            dialog.Show();
-
-            Debug.Log("Dialogo attivato");
-
-            bool result = await dialog.WaitForUserInput();
-
-            Debug.Log("Completa ShowCashDialog con: " + result);
-            return result;
         }
 
         private async Task<bool> ShowErrorDialog()
         {
-            GameObject dialogInstance = UIController.Instance._dialogs.transform.Find("ErrorDialog").gameObject;
-            ErrorDialog dialog = dialogInstance.GetComponent<ErrorDialog>();
+            var dialog = UIController.Instance._dialogs.transform
+                .Find("ErrorDialog").GetComponent<ErrorDialog>();
             dialog.Show();
-            Debug.Log("Error Dialog attivato");
-
             await Task.Delay(2000);
-
             dialog.Hide();
-
-            Debug.Log("Completa ShowErrorDialog dopo 2 secondi");
-            return true; 
+            return true;
         }
 
-        public int getPrefabIndex()
-        {
-            return _prefabIndex;
-        }
-
-        public void setPrefabIndex(int prefabIndex)
-        {
-            _prefabIndex = prefabIndex;
-        }
+        public int getPrefabIndex() => _prefabIndex;
+        public void setPrefabIndex(int prefabIndex) => _prefabIndex = prefabIndex;
     }
 }
